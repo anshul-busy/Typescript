9cc02eab82106b34911863ff5c805879f27b476d
anshul-busy <anshul@tech.busy.in>
2026-07-29 12:05:09 +0530

    some changed

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Button_p from './components/Button_p'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+<Button_p />
     </>
   )
 }
---
+++ practice/src/components/Button_p.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import "./style.css"
+const Button_p = () => {
+  return (
+    <div>
+
+        <div className="cont-btn">
+        
+                <button><FcGoogle />Google</button>
+        
+       
+            <button><FaGithub /> Google</button>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Button_p
